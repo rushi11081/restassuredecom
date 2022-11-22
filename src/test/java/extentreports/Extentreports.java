baa12375361b5cc9afb7347f37d128e3b5ab7142
rushi11081 <100134019+rushi11081@users.noreply.github.com>
2022-11-22 10:25:09 +0530

    update
    new add

--- src/test/java/extentreports/Extentreports.java
+++ src/test/java/extentreports/Extentreports.java
@@ -2,7 +2,7 @@ package extentreports;
 
 import com.aventstack.extentreports.ExtentReports;
 
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
